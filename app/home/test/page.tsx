b8a7b8faa3e4412de5ac9c2d216f0b15b40793fc
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2024-07-27 00:43:50 +0200

    Add logout button

--- app/home/test/page.tsx
+++ app/home/test/page.tsx
@@ -1,10 +1,7 @@
-import { LogoutBtn } from '@/components/auth/logout-btn';
-
 export default function Home() {
   return (
     <div className='flex min-h-screen flex-col items-center gap-y-12 p-24'>
-      <div>Page home</div>
-      <LogoutBtn />
+      <div>Page test</div>
     </div>
   );
 }
